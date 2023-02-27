02ee50889395d717ff43823a5df35cf39027a98d
Helyion <gaelbellanger283@gmail.com>
2023-02-27 14:31:25 +0100

    Done Login class / Refactor JUnit Test

--- src/main/java/io/github/helyion/login/Login.java
+++ src/main/java/io/github/helyion/login/Login.java
@@ -1,11 +1,42 @@
 package io.github.helyion.login;
 
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
 public class Login {
 
     private String mail;
     private String password;
+    private Page page;
 
-    public login(){
+    public Login(Page page, String mail, String password){
+        this.page = page;
+        this.mail = mail;
+        this.password = password;
+    }
 
+    public void Dologin(){
+        this.page.navigate("https://ztrain-web.vercel.app/auth/login");
+        this.page.getByPlaceholder("Email").click();
+        this.page.getByPlaceholder("Email").fill(this.mail);
+        this.page.getByPlaceholder("Mot de passe").click();
+        this.page.getByPlaceholder("Mot de passe").fill(this.password);
+        this.page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connexion").setExact(true)).click();
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- src/test/java/io/github/helyion/TestConnextionTest.java
+++ src/test/java/io/github/helyion/TestConnextionTest.java
@@ -2,6 +2,7 @@ package io.github.helyion;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
+import io.github.helyion.login.Login;
 import org.junit.jupiter.api.*;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
@@ -15,10 +16,12 @@ class TestConnextionTest {
     // New instance for each test method.
     BrowserContext context;
     Page page;
+    private Login login;
 
     // Mail + passworld
     private String mail = "test.mail@email.com";
     private String mdp = "motsdepasse";
+
 
     @BeforeAll
     static void launchBrowser() {
@@ -35,6 +38,7 @@ class TestConnextionTest {
     void createContextAndPage() {
         context = browser.newContext();
         page = context.newPage();
+        login = new Login(page, mail, mdp);
     }
 
     @AfterEach
@@ -56,56 +60,35 @@ class TestConnextionTest {
 
     @Test
     void testLogin() {
-        page.navigate("https://ztrain-web.vercel.app/auth/login");
-        page.getByPlaceholder("Email").click();
-        page.getByPlaceholder("Email").fill(mail);
-        page.getByPlaceholder("Mot de passe").click();
-        page.getByPlaceholder("Mot de passe").fill(mdp);
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connexion").setExact(true)).click();
+        login.Dologin();
         page.waitForURL("https://ztrain-web.vercel.app/home");
     }
 
     @Test
     void testLoginErrorPassword() {
-        page.navigate("https://ztrain-web.vercel.app/auth/login");
-        page.getByPlaceholder("Email").click();
-        page.getByPlaceholder("Email").fill(mail);
-        page.getByPlaceholder("Mot de passe").click();
-        page.getByPlaceholder("Mot de passe").fill("wrongpassword12345");
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connexion").setExact(true)).click();
+        login.setPassword("wrongpassword12345");
+        login.Dologin();
         assertThat(page.getByText("Email ou mot de passe incorrect")).isVisible();
     }
 
     @Test
     void testLoginErrorMail() {
-        page.navigate("https://ztrain-web.vercel.app/auth/login");
-        page.getByPlaceholder("Email").click();
-        page.getByPlaceholder("Email").fill("erroremail@email.com");
-        page.getByPlaceholder("Mot de passe").click();
-        page.getByPlaceholder("Mot de passe").fill(mdp);
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connexion").setExact(true)).click();
+        login.setMail("erroremail@email.com");
+        login.Dologin();
         assertThat(page.getByText("Email ou mot de passe incorrect")).isVisible();
     }
 
     @Test
     void testLoginErrorFormatMail() {
-        page.navigate("https://ztrain-web.vercel.app/auth/login");
-        page.getByPlaceholder("Email").click();
-        page.getByPlaceholder("Email").fill("erroremail");
-        page.getByPlaceholder("Mot de passe").click();
+        login.setMail("erroremail");
+        login.Dologin();
         assertThat(page.getByText("Le format de l'email est invalid")).isVisible();
     }
 
     @Test
-    void testDeConnexion() {
-        page.navigate("https://ztrain-web.vercel.app/auth/login");
-        page.getByPlaceholder("Email").click();
-        page.getByPlaceholder("Email").fill(mail);
-        page.getByPlaceholder("Mot de passe").click();
-        page.getByPlaceholder("Mot de passe").fill(mdp);
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connexion").setExact(true)).click();
+    void testDeconnexion() { //Deconnexion Impossible
+        login.Dologin();
         page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Se déconnecter")).click();
         page.waitForURL("https://ztrain-web.vercel.app/auth/login");
-        //Deconnection Impossible
     }
 }
